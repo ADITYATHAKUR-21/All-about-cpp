a7e7858183a02032ecae1a73ade5bef9b5470f3f
Aditya Kumar <adityakr123muz@gmail.com>
2025-05-15 17:31:58 +0530

    Fix Task_Five

--- 05_loops/Task_Five.cpp
+++ 05_loops/Task_Five.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main (){
 
-    string teaTypes[3] = {"Green tea ", "Black tea", "Oolong tea"};
+    string teaTypes[3] = {"Green tea", "Black tea", "Oolong tea"};
 
     for (int i = 0; i < 3; i++){
 
         if(teaTypes[i] == "Green tea"){
+
             cout << "skiping the " << teaTypes[i] << endl;
             continue;
             
