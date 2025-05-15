1f99e5dbacee4201c8bde30ab94922ac580a7e76
Aditya Kumar <adityakr123muz@gmail.com>
2025-05-15 17:55:38 +0530

    Add problem six

--- 05_loops/Task_Six.cpp
+++ 05_loops/Task_Six.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main (){
+        string teaTypes[3] = {"Green tea", "Black tea", "Oolong tea"};
+
+        for (int i = 0; i  < 3; i++){
+            cout << " Brewing " << teaTypes[i] << "..." << endl;
+            
+            for(int j = 1; j <= 3; j++){
+
+                cout << "Brewing "<< j << " cup of tea " << teaTypes[i] << endl;
+            }           
+
+
+        }
 
 return 0;
 }
