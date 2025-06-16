8ead0768c7a25e94fdea719121d3e1faebe3b8cd
Aditya Kumar <adityakr123muz@gmail.com>
2025-06-16 19:58:35 +0530

    Added array2 file

---
+++ 07_array/myarray2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int Chaiserved[7] = {50, 70, 89, 56, 78, 89, 89};
+
+    cout << "Chai served  day (1)  " << Chaiserved[0] << endl;
+    cout << "Chai served  day (2)  " << Chaiserved[1] << endl;
+    cout << "Chai served  day (3)  " << Chaiserved[2];
+
+    return 0;
+}
