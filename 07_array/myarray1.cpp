45c1a94fe24203d22c2bd929eac845253cbdf32d
Aditya Kumar <adityakr123muz@gmail.com>
2025-06-16 19:42:08 +0530

    Added array file

---
+++ 07_array/myarray1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+     int chaiTemperature[5] = {58, 95, 56, 65, 35, };
+
+     cout << "Chai Temperauter : " << endl;
+
+     for( int i = 0; i < 5; i++){
+
+        cout << chaiTemperature[i] << " degree C \n";
+    
+     } 
+
+
+    return 0;
+}
